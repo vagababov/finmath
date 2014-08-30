1ef822e9619bae1ed27105012e990c4dd7af7e67
Victor Agababov <vagababov@gmail.com>
2014-08-30 15:27:04 -0700

    more tests; more formulas for future, present and amount

--- go/finmath.go
+++ go/finmath.go
@@ -12,7 +12,7 @@ func CompoundInterest(start, periods, rate float64) float64 {
 }
 
 // PV (annuity) returns the required amount to put down in order to receive
-// rent for period times with given rate.
+// rent for period times with given rate. A.k.a Series present worth.
 // if start == 1, then interest is accrued at the start of the period.
 // E.g. to receive 1200 monthly over 15 years on an instrument that earns 4.5%
 // annually, you have to pay now:
@@ -22,19 +22,29 @@ func PV(rent, periods, rate float64) float64 {
 }
 
 // FV returns the future value of an instrument earning rate over periods
-// paying rent per period.
+// paying rent per period. A.k.a series compound amount.
 func FV(rent, periods, rate float64) float64 {
 	return (math.Pow(1.+rate, periods) - 1) / rate * rent
 }
 
-// PMT (periodic payment) returns the payment amount to repay "sum" over
-// "periods" with "rate".
+// PVG returns the required
+func PVG(gradient, periods, rate float64) float64 {
+	return 0
+}
+
+// PMT (periodic payment) returns the payment amount to repay "pv" over
+// "periods" with "rate". A.k.a capital recovery.
 // For example, PMT is the payment you have to make for a fixed-rate
 // mortgage loan.
 // For 30-year loan, paid monthly with 4.5% rate: periods = 30*12 = 360
 // rate = 0.045/12
-func PMT(sum, periods, rate float64) float64 {
-	return sum * rate / (1. - math.Pow(1.+rate, -periods))
+func PMT(pv, periods, rate float64) float64 {
+	return pv * rate / (1. - math.Pow(1.+rate, -periods))
+}
+
+// PMTF returns the
+func PMTF(fv, periods, rate float64) float64 {
+	return fv * rate / (math.Pow(1.+rate, periods) - 1)
 }
 
 // RTC rounds the given amount to the nearest cent.
--- go/finmath_test.go
+++ go/finmath_test.go
@@ -1,7 +1,10 @@
 // Contains unit tests for the finmath package.
 package finmath
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestCompoundInterest(t *testing.T) {
 	if got, want := CompoundInterest(1000, 2, 0.2), 1440.; got != want {
@@ -9,9 +12,25 @@ func TestCompoundInterest(t *testing.T) {
 	}
 }
 
+func TestFV(t *testing.T) {
+	if got, want := RTC(FV(1596.18, 360, 0.03625/12)), 1036696.15; got != want {
+		t.Errorf("RTC(FV): got: %f want: %f", got, want)
+	}
+	// Verify PV and FV relationship holds.
+	if got, want := RTC(FV(1596.18, 360, 0.03625/12)), RTC(PV(1596.18, 360, 0.03625/12)*math.Pow(1+0.03625/12, 360)); got != want {
+		t.Errorf("FV != PV*(1+i)^n: got: %f want: %f", got, want)
+	}
+}
+
 func TestPV(t *testing.T) {
 	if got, want := RTC(PV(1200, 15*12, 0.045/12)), 156864.12; got != want {
-		t.Errorf("RTC(PV): got: %v want: %v", got, want)
+		t.Errorf("RTC(PV): got: %f want: %f", got, want)
+	}
+}
+
+func TestPMT(t *testing.T) {
+	if got, want := RTC(PMT(350000, 360, 0.03625/12)), 1596.18; got != want {
+		t.Errorf("RTV(PMT): got: %f want: %f", got, want)
 	}
 }
 
@@ -32,10 +51,10 @@ func TestRoundting(t *testing.T) {
 
 	for _, test := range tests {
 		if got, want := RTC(test.v), test.wantRTC; got != want {
-			t.Errorf("RTC(%V): got: %v want: %v", test.v, got, want)
+			t.Errorf("RTC(%f): got: %f want: %f", test.v, got, want)
 		}
 		if got, want := FTC(test.v), test.wantFTC; got != want {
-			t.Errorf("FTC(%V): got: %v want: %v", test.v, got, want)
+			t.Errorf("FTC(%f): got: %f want: %f", test.v, got, want)
 		}
 	}
 }
